75483ebe5ad5b055ef6ffe35f1bcc8b25d80e7df
Mitchell Blom <mitchellblom@gmail.com>
2017-04-19 22:04:40 -0500

    bootstrap styling and comment cleanup

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,102 +1,103 @@
     var categories = [];
     var types = [];
     var products = [];
-    
-$(document).ready(function() {
 
+    $(document).ready(function() {
 
-    function writeOptionsToDOM(array, heading) {
-        var optionString = "";
-        optionString += `<h5>${heading}</h5>`;
-        optionString += `<div class="btn-group">
-        				<div class="dropdown">
- 						<button id="${heading}-button" class="btn btn-info btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">   						
- 						All
-   						<span class="caret"></span></button>
-        				<ul class="dropdown-menu" id="${heading}">`;
-        for (var i = 0; i < array.length; i++) {
+        function writeOptionsToDOM(array, heading) {
+            var optionString = "";
             optionString += 
-            				// `<li id="${heading}-${array[i].id}">
-            				// <a class="dynamicOption">${array[i].name}
-            				// </a></li>`;
-            				`<li id="${heading}-${array[i].id}"><a class="dynamicOption">${array[i].name}</a></li>`;
-        }
-        optionString += `</ul>
-       					</div></div>`;
+            			`<div class="card">
+        				<div class="btn-group">
+        				<div class="dropdown">
+ 						<button id="${heading}-button" class="btn btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">   						
+ 						${heading}
+   						<span class="caret"></span>
+   						</button>
+        				<ul class="dropdown-menu" id="${heading}">`;
+            for (var i = 0; i < array.length; i++) {
+                optionString +=
+                    	`<li id="${heading}-${array[i].id}">
+            			<a class="dynamicOption">${array[i].name}</a>
+            			</li>`;
+            }
+            optionString += 
+            			`</ul>
+       					</div>
+       					</div>
+       					</div>`;
 
-        $("#cardContainer").append(optionString);
-    };
-
-    function writeProductCardsToDOM(array) {
-        productString = "";
-        if (i % 4 === 0) {
-            productString += `<div class="row">`;
+            $("#optionContainer").append(optionString);
         };
-        for (var i = 0; i < array.length; i++) {
-            productString += `<div class="col-md-3 productCard ${array[i].type}">`;
-            productString += `<h3>${array[i].name}</h3>`;
-            productString += `<img src="${array[i].image}" class="img-circle thumbnail" alt="Product Image">`;
-            productString += `<h4>${array[i].description}</h4>`;
-            productString += `</div>`;
-            if (i % 4 === 3) {
-                productString += `</div>`
+
+        function writeProductCardsToDOM(array) {
+            productString = "";
+            if (i % 4 === 0) {
+                productString += `<div class="row">`;
             };
+            for (var i = 0; i < array.length; i++) {
+                productString += `<div class="col-md-3 productCard ${array[i].type}">
+                				<h3>${array[i].name}</h3>
+                				<img src="${array[i].image}" class="img-circle thumbnail" alt="Product Image">
+                				<h4>${array[i].description}</h4>
+                				</div>`;
+                if (i % 4 === 3) {
+                    productString += `</div>`
+                };
+            }
+            $("#cardContainer").append(productString);
         }
-        $("#cardContainer").append(productString);
-        // $(".productCard").hide();
-    }
 
-    var productJSON = function() {
-        return new Promise(function(resolve, reject) {
-            $.ajax("./db/products.json").done(function(data1) {
-                resolve(data1.products)
-            }).fail(function(error1) {
-                reject(error1);
+        var productJSON = function() {
+            return new Promise(function(resolve, reject) {
+                $.ajax("./db/products.json").done(function(data1) {
+                    resolve(data1.products)
+                }).fail(function(error1) {
+                    reject(error1);
+                });
             });
-        });
-    };
+        };
 
-    var categoryJSON = function() {
-        return new Promise(function(resolve, reject) {
-            $.ajax("./db/categories.json").done(function(data2) {
-                resolve(data2.categories);
-            }).fail(function(error2) {
-                reject(error2);
+        var categoryJSON = function() {
+            return new Promise(function(resolve, reject) {
+                $.ajax("./db/categories.json").done(function(data2) {
+                    resolve(data2.categories);
+                }).fail(function(error2) {
+                    reject(error2);
+                });
             });
-        });
-    };
+        };
 
-    var typeJSON = function() {
-        return new Promise(function(resolve, reject) {
-            $.ajax("./db/types.json").done(function(data3) {
-                resolve(data3.types);
-            }).fail(function(error3) {
-                reject(error3);
+        var typeJSON = function() {
+            return new Promise(function(resolve, reject) {
+                $.ajax("./db/types.json").done(function(data3) {
+                    resolve(data3.types);
+                }).fail(function(error3) {
+                    reject(error3);
+                });
             });
-        });
-    };
+        };
 
-    categoryJSON().then(function(results) {
-        results.forEach(function(singleCategory) {
-            categories.push(singleCategory);
+        categoryJSON().then(function(results) {
+            results.forEach(function(singleCategory) {
+                categories.push(singleCategory);
+            });
+            writeOptionsToDOM(categories, "Categories");
         });
-        writeOptionsToDOM(categories, "Categories");
+
+        typeJSON().then(function(results) {
+            results.forEach(function(singleType) {
+                types.push(singleType);
+            });
+            writeOptionsToDOM(types, "Types");
+        });
+
+        productJSON().then(function(results) {
+            results.forEach(function(singleProduct) {
+                products.push(singleProduct);
+            });
+            writeOptionsToDOM(products, "Products");
+            writeProductCardsToDOM(products);
+        });
+
     });
-
-    typeJSON().then(function(results) {
-        results.forEach(function(singleType) {
-            types.push(singleType);
-        });
-        writeOptionsToDOM(types, "Types");
-    });
-
-    productJSON().then(function(results) {
-        results.forEach(function(singleProduct) {
-            products.push(singleProduct);
-        });
-        writeOptionsToDOM(products, "Products");
-        writeProductCardsToDOM(products);
-    });
-
-    
-});
--- javascripts/views.js
+++ javascripts/views.js
@@ -1,90 +1,47 @@
 $(document).ready(function() {
 
     $("body").on("click", ".dynamicOption", function() {
-            console.log("this inner: ", this.innerHTML);
-            $(".productCard").hide();
-            var test = this.innerHTML;
-            console.log("test: ", test);
+        $(".productCard").hide();
+        var test = this.innerHTML;
 
-            var rec = [];
-            rec.push($(".0"));
-            rec.push($(".1"));
-            rec.push($(".2"));
+        var rec = [];
+        rec.push($(".0"));
+        rec.push($(".1"));
+        rec.push($(".2"));
 
-            var com = [];
-            com.push($(".3"));
-            com.push($(".4"));
-            com.push($(".5"));
+        var com = [];
+        com.push($(".3"));
+        com.push($(".4"));
+        com.push($(".5"));
 
-            var mil = [];
-            mil.push($(".6"));
-            mil.push($(".7"));
-            mil.push($(".8"));
+        var mil = [];
+        mil.push($(".6"));
+        mil.push($(".7"));
+        mil.push($(".8"));
 
-            console.log(test);
-            if (test === "Recreational") {
-                for (var i = 0; i < rec.length; i++) {
-                    $(rec[i]).show();
-                }
-            } else if (test === "Commercial") {
-                for (var j = 0; j < com.length; i = j++) {
-                    $(com[j]).show();
-                }
-            } else if (test === "Military")
-                for (var k = 0; k < mil.length; k++) {
-                    $(mil[k]).show();
-                }
+        if (test === "Recreational") {
+            for (var i = 0; i < rec.length; i++) {
+                $(rec[i]).show();
+            }
+        } else if (test === "Commercial") {
+            for (var j = 0; j < com.length; i = j++) {
+                $(com[j]).show();
+            }
+        } else if (test === "Military")
+            for (var k = 0; k < mil.length; k++) {
+                $(mil[k]).show();
+            }
 
-            // if (test === "Recreational") {
-            //     for (var i = 0; i < rec.length; i++) {
-            //         $(rec[i]).show();
-            //     }
-            // } else if (test === "Commercial") {
-            //     for (var j = 0; j < com.length; i = j++) {
-            //         $(com[j]).show();
-            //     }
-            // } else if (test === "Military")
-            //     for (var k = 0; k < mil.length; k++) {
-            //         $(mil[k]).show();
-            //     }
-
-            var typeFilteredProducts = [];
-
-            var typeID = $(this).parent().attr("id");
-            console.log("typeID", typeID);
-            var typeNumber = typeID.split("Types-");
-            console.log("typeNumber[1]", typeNumber[1]);
-			for (var l = 0; l < products.length; l++) {
-				console.log("products[l].type: ", products[l].type);
-                    if (products[l].type == typeNumber[1]){
-                    	console.log("found match: ", products[l].type);
-                    	typeFilteredProducts.push(products[l])
-                    }
-                }
-                    	console.log(typeFilteredProducts);
-                	}
-			);
-        // for (var i = 0; i < com.length; i++){
-        // 	console.log("com i: ", com[i]);
-        // 	console.log($(com[i]));
-        // 	$(com[i]).show();
-        // }
-        // this.childNodes.nodeValue
-        // forEach or .class("Recreational");
-        // if (test === "Recreational") {
-        //     console.log("rec");
-        //     $(".productCard").hide();
-        //     console.log($(".0"));
-        //     $(".0").forEach().show();
-        // } else if (test === "Commercial") {
-        //     console.log("com");
-        //     $(".productCard").hide();
-        //     $(".1").show();
-        // } else if (test === "Military") {
-        //     console.log("mil");
-        //     $(".productCard").hide();
-        //     $(".2").show();
-        // }
-    
+        var typeID = $(this).parent().attr("id");
+        var typeNumber = typeID.split("Types-");
+        var filteredType = typeNumber[1];
+        console.log("Type Number: ", typeNumber[1]);
+        for (var l = 0; l < products.length; l++) {
+            if (products[l].type == typeNumber[1]) {
+                // console.log("found match: ", products[l]);
+                products[l].show();
+            }
+        }
+    });
 
 });
