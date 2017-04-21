d4b46f0ddcd900c22b6b5a3910767334025003aa
Mitchell Blom <mitchellblom@gmail.com>
2017-04-20 19:13:20 -0500

    types rewriting to dom properly - will apply same solution to other categories

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,8 +6,8 @@
 
         function writeOptionsToDOM(array, heading) {
             var optionString = "";
-            	optionString += 
-            			`<div class="card">
+            optionString +=
+                `<div class="card">
         				<div class="btn-group">
         				<div class="dropdown">
  						<button id="${heading}-button" class="btn btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">   						
@@ -17,12 +17,12 @@
         				<ul class="dropdown-menu" id="${heading}">`;
             for (var i = 0; i < array.length; i++) {
                 optionString +=
-                    	`<li id="${heading}-${array[i].id}">
+                    `<li id="${heading}-${array[i].id}">
             			<a class="dynamicOption">${array[i].name}</a>
             			</li>`;
             }
-            	optionString += 
-            			`</ul>
+            optionString +=
+                `</ul>
        					</div>
        					</div>
        					</div>`;
@@ -30,6 +30,7 @@
         };
 
         function writeProductCardsToDOM(array) {
+            $("#cardContainer").html("");
             productString = "";
             if (i % 4 === 0) {
                 productString += `<div class="row">`;
@@ -99,5 +100,69 @@
             writeOptionsToDOM(products, "Products");
             writeProductCardsToDOM(products);
         });
+
+        ////////////////
+
+        $("body").on("click", ".dynamicOption", function() {
+            $(".productCard").hide();
+
+            // Category view filter
+            var categoryClicked = this.innerHTML;
+
+            var rec = [];
+            rec.push($(".0"));
+            rec.push($(".1"));
+            rec.push($(".2"));
+
+            var com = [];
+            com.push($(".3"));
+            com.push($(".4"));
+            com.push($(".5"));
+
+            var mil = [];
+            mil.push($(".6"));
+            mil.push($(".7"));
+            mil.push($(".8"));
+
+            if (categoryClicked === "Recreational") {
+                for (var i = 0; i < rec.length; i++) {
+                    // $(rec[i]).show();
+                	writeProductCardsToDOM($(rec[i]));
+                }
+            } else if (categoryClicked === "Commercial") {
+                for (var j = 0; j < com.length; i = j++) {
+                    $(com[j]).show();
+                }
+            } else if (categoryClicked === "Military")
+                for (var k = 0; k < mil.length; k++) {
+                    $(mil[k]).show();
+                }
+
+            // Type view filter
+
+            var clickedID = $(this).parent().attr("id");
+            var typeNumber = clickedID.split("Types-");
+            var filteredType = typeNumber[1];
+            var filteredProductsByType = [];
+
+            for (var l = 0; l < products.length; l++) {
+                if (products[l].type == filteredType) {
+                    filteredProductsByType.push(products[l]);
+                }
+                writeProductCardsToDOM(filteredProductsByType);
+            }
+
+            // Product view filter
+            var specificProductClicked = $(this).html();
+            for (var n = 0; n < products.length; n++) {
+                if (products[n].name == specificProductClicked) {
+
+                }
+            }
+
+
+        });
+
+
 
     });
--- javascripts/views.js
+++ javascripts/views.js
@@ -1,73 +1,74 @@
-$(document).ready(function() {
+// // $(document).ready(function() {
 
-    $("body").on("click", ".dynamicOption", function() {
-        $(".productCard").hide();
+//     $("body").on("click", ".dynamicOption", function() {
+//         $(".productCard").hide();
 
-        // Category view filter
-        var categoryClicked = this.innerHTML;
+//         // Category view filter
+//         var categoryClicked = this.innerHTML;
 
-        var rec = [];
-        rec.push($(".0"));
-        rec.push($(".1"));
-        rec.push($(".2"));
+//         var rec = [];
+//         rec.push($(".0"));
+//         rec.push($(".1"));
+//         rec.push($(".2"));
 
-        var com = [];
-        com.push($(".3"));
-        com.push($(".4"));
-        com.push($(".5"));
+//         var com = [];
+//         com.push($(".3"));
+//         com.push($(".4"));
+//         com.push($(".5"));
 
-        var mil = [];
-        mil.push($(".6"));
-        mil.push($(".7"));
-        mil.push($(".8"));
+//         var mil = [];
+//         mil.push($(".6"));
+//         mil.push($(".7"));
+//         mil.push($(".8"));
 
-        if (categoryClicked === "Recreational") {
-            for (var i = 0; i < rec.length; i++) {
-                $(rec[i]).show();
-            }
-        } else if (categoryClicked === "Commercial") {
-            for (var j = 0; j < com.length; i = j++) {
-                $(com[j]).show();
-            }
-        } else if (categoryClicked === "Military")
-            for (var k = 0; k < mil.length; k++) {
-                $(mil[k]).show();
-            }
+//         if (categoryClicked === "Recreational") {
+//             for (var i = 0; i < rec.length; i++) {
+//                 // $(rec[i]).show();
+//                 writeProductCardsToDOM(rec);
+//             }
+//         } else if (categoryClicked === "Commercial") {
+//             for (var j = 0; j < com.length; i = j++) {
+//                 $(com[j]).show();
+//             }
+//         } else if (categoryClicked === "Military")
+//             for (var k = 0; k < mil.length; k++) {
+//                 $(mil[k]).show();
+//             }
 
-        // Type view filter
-        var allProductsOnDom = $(".productCard");
+//         // Type view filter
+//         var allProductsOnDom = $(".productCard");
 
-        var typeID = $(this).parent().attr("id");
-        var typeNumber = typeID.split("Types-");
-        var filteredType = typeNumber[1];
-        var filteredProducts = [];
+//         var typeID = $(this).parent().attr("id");
+//         var typeNumber = typeID.split("Types-");
+//         var filteredType = typeNumber[1];
+//         var filteredProducts = [];
 
-        for (var l = 0; l < products.length; l++) {
-            if (products[l].type == filteredType) {
-                // console.log("found type match: ", products[l]);
-                filteredProducts.push(products[l]);
-                // console.log("filteredProducts: ", filteredProducts);
-                for (var m = 0; m < allProductsOnDom.length; m++) {
-                    console.log("filteredProducts[m]: ", filteredProducts[m]);
-                    // $(filteredProducts[m]).show();
-                    // console.log("allProductsOnDom[m]: ", $("allProductsOnDom[m]"));
-					$(allProductsOnDom[m]).show();
-                    // if (allProductsOnDom[m].id == filteredProducts[m].id) {
-                        // $("allProductsOnDom[m]").show();
-                    // }
-                }
-                // }
-                // products[l].show();					// where i want to show the products that match type
-            }
-        }
+//         for (var l = 0; l < products.length; l++) {
+//             if (products[l].type == filteredType) {
+//                 // console.log("found type match: ", products[l]);
+//                 filteredProducts.push(products[l]);
+//                 // console.log("filteredProducts: ", filteredProducts);
+//                 for (var m = 0; m < allProductsOnDom.length; m++) {
+//                     console.log("filteredProducts[m]: ", filteredProducts[m]);
+//                     // $(filteredProducts[m]).show();
+//                     // console.log("allProductsOnDom[m]: ", $("allProductsOnDom[m]"));
+// 					$(allProductsOnDom[m]).show();
+//                     // if (allProductsOnDom[m].id == filteredProducts[m].id) {
+//                         // $("allProductsOnDom[m]").show();
+//                     // }
+//                 }
+//                 // }
+//                 // products[l].show();					// where i want to show the products that match type
+//             }
+//         }
 
-        // Product view filter
-        var specificProductClicked = $(this).html();
-        for (var n = 0; n < products.length; n++) {
-            if (products[n].name == specificProductClicked) {
-                // console.log("found product match: ", products[n]);
-                // products[n].show();			// where i want to show the individual product selected
-            }
-        }
-    });
-});
+//         // Product view filter
+//         var specificProductClicked = $(this).html();
+//         for (var n = 0; n < products.length; n++) {
+//             if (products[n].name == specificProductClicked) {
+//                 // console.log("found product match: ", products[n]);
+//                 // products[n].show();			// where i want to show the individual product selected
+//             }
+//         }
+//     });
+// // });
